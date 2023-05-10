62d9b26b7e5b342229caa1fc2a94ed933b7ba575
EdwinSch <edwinschutjes@gmail.com>
2023-05-10 15:20:01 +0200

    add: add new widgets button and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, Button } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 
 import Widget from "./components/Widget";
 import widgets from "./widgets.js";
@@ -7,13 +8,53 @@ import widgets from "./widgets.js";
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [widgetsList, setWidgetsList] = useState(widgets);
+  const [listLength, setListLength] = useState(false);
+
+  const addWidget = () => {
+    const newWidgetsList = widgets.push({
+      name: "adipisicing",
+      type: "files",
+      content:
+        "Blackbird spyplane cred schlitz, vinyl sustainable mukbang narwhal bruh actually deep v neutral milk hotel celiac grailed pitchfork.",
+    });
+    // console.log(widgets);
+    setWidgetsList(newWidgetsList);
+
+    if (widgets.length >= 10) {
+      setListLength(true);
+    }
+  };
 
   return (
-    <Box as="main" p={3}>
+    <Box as="main" p={4}>
+      {listLength ? (
+        <Button
+          isDisabled={listLength}
+          type="button"
+          colorScheme="teal"
+          mb={6}
+          size="sm"
+        >
+          Maximum widgets reached
+        </Button>
+      ) : (
+        <Button
+          isDisabled={listLength}
+          type="button"
+          onClick={addWidget}
+          colorScheme="teal"
+          mb={6}
+          size="sm"
+          leftIcon={<AddIcon />}
+        >
+          Add new widget
+        </Button>
+      )}
+
       <Flex flexWrap="wrap" gap={5}>
-        {widgets.map((widget) => {
-          return <Widget key={widget.id} {...widget} />;
+        {widgets.map((widget, index) => {
+          return <Widget key={index} {...widget} />;
         })}
       </Flex>
     </Box>
--- src/widgets.js
+++ src/widgets.js
@@ -1,38 +1,27 @@
 const widgets = [
   {
-    id: 1,
     name: "lorem",
     type: "schedule",
     content:
       "Flexitarian migas swag authentic pitchfork green juice skateboard irony ramps craft beer butcher unicorn.",
   },
   {
-    id: 2,
     name: "ipsum",
     type: "progress",
     content:
       "Snackwave tonx pop-up hell of pour-over flannel aesthetic gentrify tumeric celiac.",
   },
   {
-    id: 3,
     name: "dolor",
     type: "weather",
     content:
       "Aporware la croix heirloom austin pinterest humblebrag jawn edison bulb meggings tofu ramps synth celiac street art big mood.",
   },
   {
-    id: 4,
     name: "consectetur",
     type: "agenda",
     content:
       "Shaman iceland enamel pin banjo bitters jean shorts. Flannel mukbang gluten-free hella.",
-  },
-  {
-    id: 5,
-    name: "adipisicing",
-    type: "files",
-    content:
-      "Blackbird spyplane cred schlitz, vinyl sustainable mukbang narwhal bruh actually deep v neutral milk hotel celiac grailed pitchfork.",
   },
 ];
 
